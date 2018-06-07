6290eed1abc9f23b132018e490a96160d9b2c1ec
lakessh <kumarlakessh@gmail.com>
2018-06-07 17:57:57 +0800

    Added places api key

--- app.js
+++ app.js
@@ -11,12 +11,37 @@ server.use(bodyParser.urlencoded({extended:true}));
 server.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
+const PLACES_API_KEY = 'AIzaSyCXYaMh3-lkp43HqsbvxC-DYG55SgQNbf8';
+
 server.get ('/form',(req,res)=>{
   res.render('form.hbs');
 });
 
 server.get ('/',(req,res)=>{
   res.render('home.hbs');
+});
+
+server.post('/getplaces',(req,res) => {
+  const addr = req.body.address;
+  const locationReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyA9HmIKh3Yv-T9zS_JxjMZb8ZpEOpq7AtQ`;
+
+axios.get(locationReq).then((response) => {
+  const locationData = {
+    addr:response.data.results[0].formatted_address,
+    lat:response.data.results[0].geometry.location.lat,
+    lng:response.data.results[0].geometry.location.lng,
+  }
+
+const placesReq = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationData.lat},${locationData.lng}&radius=1500&types=food&name=food&key=AIzaSyCXYaMh3-lkp43HqsbvxC-DYG55SgQNbf8=${PLACES_API_KEY}`;
+
+  console.log(locationData);
+  res.status(200).send(locationData);
+
+}).catch((error)=>{
+  console.log(error);
+});
+
+
 });
 
 server.listen(port,()=>{
